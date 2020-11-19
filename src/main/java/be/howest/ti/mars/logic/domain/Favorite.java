a36e5442c9988c9bf125aa42d4729fe87304b96c
robin <robin.de.kinders@student.howest.be>
2020-11-19 18:20:14 +0100

    added equals and hashcode in Favorite class

--- src/main/java/be/howest/ti/mars/logic/domain/Favorite.java
+++ src/main/java/be/howest/ti/mars/logic/domain/Favorite.java
@@ -2,6 +2,7 @@ package be.howest.ti.mars.logic.domain;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Objects;
 
 public class Favorite {
     private List<Product> favorites;
@@ -9,5 +10,18 @@ public class Favorite {
 
     public Favorite(int favoriteId) {
         this.favorites = new LinkedList<>();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Favorite favorite = (Favorite) o;
+        return favoriteId == favorite.favoriteId;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(favoriteId);
     }
 }
