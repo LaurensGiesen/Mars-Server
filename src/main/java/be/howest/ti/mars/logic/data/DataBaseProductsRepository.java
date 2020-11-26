f00bd0f4b37b94938c785ae395385922adf69e76
Laurens Giesen <laurens.giesen@student.howest.be>
2020-11-26 18:51:50 +0100

    findProduct works successfully in the Database

--- src/main/java/be/howest/ti/mars/logic/data/DataBaseProductsRepository.java
+++ src/main/java/be/howest/ti/mars/logic/data/DataBaseProductsRepository.java
@@ -14,6 +14,7 @@ public class DataBaseProductsRepository implements ProductsRepository {
     private static final Logger LOGGER = Logger.getLogger(DataBaseProductsRepository.class.getName());
     private static final String SQL_SELECT_ALL_PRODUCTS = "select * from products";
     private static final String SQL_ADD_PRODUCT = "insert into products(name, price, amount, date, image) values(?,?,?,?,?)";
+    private static final String SQL_FIND_PRODUCT = "select * from products where name like (?)";
     @Override
     public void addPlant(Plant plant) {
         try (Connection con = MarsRepository.getConnection();
@@ -56,12 +57,33 @@ public class DataBaseProductsRepository implements ProductsRepository {
             return allProducts;
         } catch (SQLException ex) {
             LOGGER.log(Level.SEVERE, ex.getMessage());
-            throw new SeedException("Unable to get all Seeds");
+            throw new SeedException("Unable to get all products");
         }
     }
 
     @Override
-    public List<Product> findProduct(String name) {
-        return null;
+    public List<Product> findProduct(String ch) {
+        try (Connection con = MarsRepository.getConnection();
+             PreparedStatement stmt = con.prepareStatement(SQL_FIND_PRODUCT)
+
+        ) { stmt.setString(1, ch);
+            try (ResultSet rs = stmt.executeQuery()) {
+                List<Product> productsByname = new ArrayList<>();
+                while (rs.next()) {
+                    int productId = rs.getInt("product_id");
+                    String name = rs.getString("name");
+                    double price = rs.getDouble("price");
+                    int amount = rs.getInt("amount");
+                    Date date = rs.getDate("date");
+                    String image = rs.getString("image");
+                    Product product = new Plant(productId, name, price,date,amount, image);
+                    productsByname.add(product);
+                }
+                return productsByname;
+            }
+        } catch (SQLException ex) {
+            LOGGER.log(Level.SEVERE, ex.getMessage());
+            throw new PlantException("Unable to find products");
+        }
     }
 }
