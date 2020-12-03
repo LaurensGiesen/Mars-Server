a34206519dc6fd20cb20da07021a8a4ddd760394
robin <robin.de.kinders@student.howest.be>
2020-12-03 15:25:58 +0100

    Added functions to Shop class

--- src/main/java/be/howest/ti/mars/logic/domain/Shop.java
+++ src/main/java/be/howest/ti/mars/logic/domain/Shop.java
@@ -1,5 +1,28 @@
 package be.howest.ti.mars.logic.domain;
 
+import be.howest.ti.mars.logic.exceptions.SeedException;
+
+import java.util.LinkedList;
+import java.util.List;
+
 public class Shop {
 
+    private List<Seed> seeds;
+
+    public Shop() {
+        this.seeds = new LinkedList<>();
+    }
+
+    public void addSeed(Seed seed) {
+        seeds.add(seed);
+    }
+
+    public boolean buySeed(Seed seed, int quantity) {
+        if (seed.getAmount() < quantity) {
+            throw new SeedException("There are not enough seeds in stock.");
+        } else {
+            seeds.remove(seed);
+        }
+        return true;
+    }
 }
