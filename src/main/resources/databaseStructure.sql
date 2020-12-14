298169ba0e45a74cd961a3986fa73843f50c2edd
Laurens Giesen <laurens.giesen@student.howest.be>
2020-12-14 15:36:30 +0100

    automatisch inladen van een aantal producten voor een nieuwe user

--- src/main/resources/databaseStructure.sql
+++ src/main/resources/databaseStructure.sql
@@ -1,1 +1,113 @@
-//drop table statements + creëren van de juiste tabellen
+drop table if exists addresses, baskets, favorites, harvests, histories, plants, seeds, subscriptions, users, users_harvests, users_histories;
+--drop table if exists addresses;
+--drop table if exists baskets;
+--drop table if exists favorites;
+--drop table if exists harvests;
+--drop table if exists histories;
+--drop table if exists plants;
+--drop table if exists seeds;
+--drop table if exists subscriptions;
+--drop table if exists users;
+--drop table if exists users_harvests;
+--drop table if exists users_histories;
+
+create table subscriptions(
+id int auto_increment not null,
+name enum('free', 'basic', 'premium') not null,
+price double not null,
+primary key (id)
+);
+
+create table addresses (
+id int auto_increment not null ,
+street nvarchar(50) not null,
+number int not null,
+dome nvarchar(50) not null,
+primary key (id)
+);
+
+create table users(
+userid int auto_increment not null,
+firstname nvarchar(255) not null,
+lastname nvarchar(255) not null,
+email nvarchar(255) not null,
+date_of_birth nvarchar(255) not null,
+subscription_id int not null,
+address_id int not null,
+primary key (userid),
+foreign key (subscription_id) references subscriptions(id)
+);
+
+create table seeds(
+id int auto_increment not null,
+name varchar(255) not null,
+price double not null,
+owner_id int not null,
+date nvarchar(255) not null,
+amount int not null,
+image nvarchar(1024),
+primary key (id),
+foreign key (owner_id) references users(userid)
+);
+
+create table plants(
+id int auto_increment not null,
+name varchar(255) not null,
+price double not null,
+owner_id int not null,
+date nvarchar(255) not null,
+amount int not null,
+image nvarchar(1024),
+primary key (id),
+foreign key (owner_id) references users(userid)
+);
+
+create table histories(
+history_id int auto_increment not null,
+plant_id int,
+seed_id int,
+date date not null,
+status varchar(255) not null,
+count int not null,
+primary key (history_id),
+foreign key (plant_id) references seeds(id),
+foreign key (seed_id) references plants(id)
+);
+
+create table harvests (
+harvestid int auto_increment not null,
+plantid int not null,
+amount int not null,
+primary key (harvestid),
+foreign key (plantid) references plants(id)
+);
+create table users_harvests(
+harvestid int not null,
+userid int not null,
+foreign key (harvestid) references harvests(harvestid),
+foreign key (userid) references users(userid)
+);
+create table users_histories(
+historyid int not null,
+userid int not null,
+foreign key (historyid) references histories(history_id),
+foreign key (userid) references users(userid)
+);
+
+create table baskets(
+id int auto_increment not null,
+user_id int not null,
+product_id int not null,
+product_type enum('seed', 'plant') not null,
+foreign key (user_id) references users(userid),
+foreign key (product_id) references seeds(id),
+foreign key (product_id) references plants(id)
+);
+
+create table favorites(
+id int auto_increment not null,
+user_id int not null,
+product_id int not null,
+product_type enum('seed', 'plant') not null,
+foreign key (user_id) references users(userid)
+);
--- src/main/resources/populateDatabase.sql
+++ src/main/resources/populateDatabase.sql
@@ -1,1 +1,12 @@
-//gewenste inserts
+insert  into subscriptions(id, name, price) values(1,'FREE',0);
+insert  into subscriptions(id, name, price) values(2,'BASIC',10);
+insert  into subscriptions(id, name, price) values(3,'PREMIUM',100);
+
+insert into users(firstname, lastname,email,date_of_birth, subscription_id,address_id) values('Kurt','Sys', 'Kurt.Sys@hotmail.com','20-05-2030',3,-1);
+
+insert into plants(name, price,owner_id, date, amount) values('Apple',5,1,DATE '2052-08-20',5);
+insert into plants(name, price,owner_id, date, amount) values('Carrot',3,1,DATE '2052-05-11',15);
+insert into plants(name, price,owner_id, date, amount) values('Cucumber',4,1,DATE '2052-07-09', 5);
+insert into plants(name, price,owner_id, date, amount) values('Tomato', 6,1, DATE '2052-06-05', 12);
+insert into plants(name, price,owner_id, date, amount) values('Pear', 3,1, DATE '2052-03-03', 15);
+insert into plants(name, price,owner_id, date, amount) values('Kiwi', 6,1, DATE '2052-02-06', 12);
